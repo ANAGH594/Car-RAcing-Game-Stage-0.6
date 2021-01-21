2f8ffd8ad346869af9e249c7db65c8c5deebc48e
ANAGH NAG <75021369+ANAGH594@users.noreply.github.com>
2021-01-21 17:48:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var canvas, backgroundImage;
+
+var allplayers;
+
+var distance=0
+ 
+var gameState = 0;
+var playerCount;
+
+var database;
+
+var form, player, game;
+
+
+function setup(){
+  canvas = createCanvas(400,400);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+}
